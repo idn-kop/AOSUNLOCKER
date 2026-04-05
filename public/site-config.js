1240bac25f3175d391523436a5c9614cf9fe2f28
idn-gop <varuuel@gmail.com>
2026-04-06 01:16:59 +0700

    Point admin shortcut at dev web app

--- public/site-config.js
+++ public/site-config.js
@@ -1,4 +1,4 @@
 window.AOSUNLOCKER_CONFIG = {
   appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw7IGutAzQjbR8X4xFuejIlPEuR2VeR8c9MRFdQ5fsbPC_EJvGffMWDK4HTuOF8Vp5z/exec',
-  adminUrl: 'https://script.google.com/macros/s/AKfycbzABqD1kPFxLLKFzsCu74TyL2JtcHOq7wMoI-BDYidhSnoN84X3efE01iW4-Bz-ffx9dg/exec',
+  adminUrl: 'https://script.google.com/macros/s/AKfycbylR6DpQC5xqjD-Rou06s0_Xdwchim37ApP5kG5gHxF/dev',
 };
