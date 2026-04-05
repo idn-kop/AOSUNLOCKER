28c5670fb76c80192f3e8b63cb498be5b44a993f
idn-gop <varuuel@gmail.com>
2026-04-06 00:50:23 +0700

    Update admin shortcut to latest deployment

--- public/site-config.js
+++ public/site-config.js
@@ -1,4 +1,4 @@
 window.AOSUNLOCKER_CONFIG = {
   appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw7IGutAzQjbR8X4xFuejIlPEuR2VeR8c9MRFdQ5fsbPC_EJvGffMWDK4HTuOF8Vp5z/exec',
-  adminUrl: 'https://script.google.com/macros/s/AKfycbyk69hMxpeO56hRnXSMkACwOZBuBhrjHU331mZKHt9bM58suv3AeHikM58ERceYepammA/exec',
+  adminUrl: 'https://script.google.com/macros/s/AKfycbzenitiMy7n-mORMbS3kgu1ibcc6j0yav5Kr3kF7qLq6dazK38GdL0CobUB_rbGqUIP8g/exec',
 };
