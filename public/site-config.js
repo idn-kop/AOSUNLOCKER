be1e92cb9bb41ace3cd63e9d1af524e83b024569
idn-gop <varuuel@gmail.com>
2026-04-05 22:01:33 +0700

    Update public Apps Script endpoint

--- public/site-config.js
+++ public/site-config.js
@@ -1,3 +1,3 @@
 window.AOSUNLOCKER_CONFIG = {
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwAGtdE7ibDmyi9yqvxiwtopiAGS7uGzyr1ZADVV62-2EhmJ--n1BEr7RWEUmLkfh4W/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwJqsivJlesHMw02wwxtCm1_rSeaIftAhj5vADdoPaGNJJ4AHahmrQWAsfVqD2d3xq2/exec',
 };
