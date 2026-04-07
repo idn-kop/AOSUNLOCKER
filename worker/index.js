594da031d5a03c85264b3b2035954d0362962cde
idn-gop <varuuel@gmail.com>
2026-04-07 14:44:22 +0700

    Redirect www domain to apex

--- worker/index.js
+++ worker/index.js
@@ -1,10 +1,18 @@
 import { onRequest } from '../functions/api/[[path]].js';
 
 const isApiRequest = (pathname) => pathname === '/api' || pathname.startsWith('/api/');
+const CANONICAL_HOST = 'aosunlocker.com';
+const LEGACY_HOSTS = new Set(['www.aosunlocker.com']);
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
+
+    if (LEGACY_HOSTS.has(url.hostname)) {
+      url.hostname = CANONICAL_HOST;
+      url.protocol = 'https:';
+      return Response.redirect(url.toString(), 308);
+    }
 
     if (isApiRequest(url.pathname)) {
       return onRequest({
